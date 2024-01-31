d85f13f8fd925908e2876c12329b1b63f2b9326f
dkinney <dkinney@dkinney.com>
2024-01-31 11:33:36 -0500

    Original Assignment

---
+++ pythonAssignment.py
@@ -0,0 +1,9 @@
+# Python Assignment
+#
+# Write python code for each problem and commit to the notebook repository on GitHub.
+# 1. Create a list of 5 fruits and print the list.
+# 2. Create a list of 5 integers and print the list.
+# 3. Ask for input from the user for a number and respond whether it is odd or even.
+# 4. Ask for 2 numbers and respond with them being added together.
+
+# Enter your code here...
