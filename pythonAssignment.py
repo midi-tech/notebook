e3efb6b058baf682d5cc87476e4fffffebd804b7
Ahmad Farawati <ahmad.farawati@midienterprises.com>
2024-01-31 12:29:55 -0600

    first assignment Ahmad

--- pythonAssignment.py
+++ pythonAssignment.py
@@ -1,14 +1,56 @@
 # Python Assignment
-#
+
 # Write python code for each problem and commit to the notebook repository on GitHub.
-# 1. Create a list of 5 fruits and print the list.
-# 2. Create a list of 5 integers and print the list.
-# 3. Ask for input from the user for a number and respond whether it is odd or even.
-# 4. Ask for 2 numbers and respond with them being added together.
 
 # Enter your code here...
 
 
 
+# 1. Create a list of 5 fruits and print the list.
 
-print('hello world')
+Mylist=["orang",'apples','mango','banana','pear']
+
+print(Mylist)
+
+
+
+
+
+# 2. Create a list of 5 integers and print the list.
+
+intlist= (1,2,3,4,5)
+print(intlist)
+
+
+
+
+
+# 3. Ask for input from the user for a number and respond whether it is odd or even.
+
+
+m =int(input('pleas enter a number'))
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+if (m % 2) == 0:
+     print("{0} is Even".format(m))
+else:
+   print("{0} is Odd".format(m))
+
+
+
+
+
+# 4. Ask for 2 numbers and respond with them being added together.
+   
+p= input('please enter the first number')
+l=input('pleas enter the second number')
+
+sum=int(p)+int(l)
+
+
+print('your two numbers added together are :',sum)
+
+
+
+
+
