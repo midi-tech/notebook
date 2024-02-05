7ffafb68c556beac30ce1b0903ac4bd5d8b666f9
Ahmad Farawati <ahmad.farawati@midienterprises.com>
2024-02-05 09:26:43 -0600

    edited assignment

--- pythonAssignment.py
+++ pythonAssignment.py
@@ -6,11 +6,18 @@
 
 
 
+
+
+
 # 1. Create a list of 5 fruits and print the list.
 
 Mylist=["orang",'apples','mango','banana','pear']
 
 print(Mylist)
+
+
+
+
 
 
 
@@ -25,28 +32,33 @@ print(intlist)
 
 
 
+
+
+
+
 # 3. Ask for input from the user for a number and respond whether it is odd or even.
 
 
-m =int(input('pleas enter a number'))
+thenumberenteredfromtheuser=int(input('please enter a number:  '))
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
-if (m % 2) == 0:
-     print("{0} is Even".format(m))
+
+if (thenumberenteredfromtheuser% 2) == 0:
+     print("{0} is Even".format(thenumberenteredfromtheuser))
 else:
-   print("{0} is Odd".format(m))
+   print("{0} is Odd".format(thenumberenteredfromtheuser))
 
 
 
 
 
-# 4. Ask for 2 numbers and respond with them being added together.
+
+
+thefirstnumberenterd= input('please enter the first number:  ')
+thesecondnumberenterd=input('pleas enter the second number:  ')
    
-p= input('please enter the first number')
-l=input('pleas enter the second number')
 
-sum=int(p)+int(l)
-
+sum=int(thefirstnumberenterd)+int(thesecondnumberenterd)
 
 print('your two numbers added together are :',sum)
 
@@ -54,3 +66,73 @@ print('your two numbers added together are :',sum)
 
 
 
+
+
+
+
+
+
+
+
+# thefirstnumberenterd= input('please enter the first number:  ')
+# thesecondnumberenterd=input('pleas enter the second number:  ')
+   
+# sum=int(thefirstnumberenterd)+int(thesecondnumberenterd)
+
+# print('your two numbers added together are :',sum)
+
+# if thefirstnumberenterd  or thesecondnumberenterd == ():
+#    print('you didnt enter a number you enterd a letter !!')
+# else:
+#    #thefirstnumberenterd= input('please enter the first number:  ')
+#    #thesecondnumberenterd=input('pleas enter the second number:  ')
+#    sum=int(thefirstnumberenterd)+int(thesecondnumberenterd)
+#    print('the total of two numbers added together are:  ',sum)
+
+
+
+
+
+
+
+
+    
+
+
+
+# try:
+#     thefirstiuputtheuserenterd=input("please enter the first number:  ")
+#     thesecondinputuserenterd=input("pleas enter the second number:  ")
+# except:
+#     thefirstiuputtheuserenterd ==(str)
+#     print("you didnt enter a number!!!")
+    
+# if thefirstiuputtheuserenterd and thesecondinputuserenterd == (int):
+#      sum= (thefirstiuputtheuserenterd)+(thesecondinputuserenterd)
+#      print("the total of the two numbers are:  ",sum)
+
+     
+
+
+
+
+
+
+
+
+
+
+
+# thefirstnumberenterd= input('please enter the first number:  ')
+# thesecondnumberenterd=input('pleas enter the second number:  ')
+# upperCaseAlphabets=None
+# sum=(thefirstnumberenterd)+(thesecondnumberenterd)
+#if thefirstnumberenterd  or thesecondnumberenterd == (upperCaseAlphabets or str or chr )  :
+#     print('you didnt enter a number you enterd a letter !!')
+
+# else:
+   
+# # #  thefirstnumberenterd= input('please enter the first number:  ')
+# # #  thesecondnumberenterd=input('pleas enter the second number:  ')
+#     sum=(thefirstnumberenterd)+(thesecondnumberenterd)
+#     print('the total of two numbers added together are:  ',sum)
